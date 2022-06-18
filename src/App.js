cc0fbdecc99bb0f4efd4ab2ba85b0fee5c323270
Samir <samirshuvo047@gmail.com>
2022-06-18 06:41:50 +0600

    firstone

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import { Switch, Route, Redirect } from "react-router-dom";
+import Home from "./pages/Home";
+import Tables from "./pages/Tables";
+import QueryBot from "./pages/Querybot";
+import ShopBot from "./pages/Shopbot";
+import Visualy from "./pages/Visualy";
+import Predicto from "./pages/Predicto";
+import Rtl from "./pages/Rtl";
+import Profile from "./pages/Profile";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Main from "./components/layout/Main";
+import "antd/dist/antd.css";
+import "./assets/styles/main.css";
+import "./assets/styles/responsive.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Switch>
+        <Route path="/sign-up" exact component={SignUp} />
+        <Route path="/sign-in" exact component={SignIn} />
+        <Main>
+          <Route exact path="/dashboard" component={Home} />
+          <Route exact path="/tables" component={Tables} />
+          <Route exact path="/shopbot" component={ShopBot} />
+          <Route exact path="/querybot" component={QueryBot} />
+          <Route exact path="/visualy" component={Visualy} />
+          <Route exact path="/predicto" component={Predicto} />
+          <Route exact path="/rtl" component={Rtl} />
+          <Route exact path="/profile" component={Profile} />
+          <Redirect from="*" to="/dashboard" />
+        </Main>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/layout/Footer.js
@@ -0,0 +1,67 @@
+import { Layout, Row, Col } from "antd";
+import { HeartFilled } from "@ant-design/icons";
+
+function Footer() {
+  const { Footer: AntFooter } = Layout;
+
+  return (
+    <AntFooter style={{ background: "#fafafa" }}>
+      <Row className="just">
+        <Col xs={24} md={12} lg={12}>
+          <div className="copyright">
+            © 2022, made with
+            {<HeartFilled />} by
+            <a href="#pablo" className="font-weight-bold" target="_blank">
+              Group Three
+            </a>
+            for a better web.
+          </div>
+        </Col>
+        <Col xs={24} md={12} lg={12}>
+          <div className="footer-menu">
+            <ul>
+              <li className="nav-item">
+                <a
+                  href="#pablo"
+                  className="nav-link text-muted"
+                  target="_blank"
+                >
+                  Shopbot
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  href="#pablo"
+                  className="nav-link text-muted"
+                  target="_blank"
+                >
+                  Querybot
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  href="#pablo"
+                  className="nav-link text-muted"
+                  target="_blank"
+                >
+                  Visualy
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  href="#pablo"
+                  className="nav-link pe-0 text-muted"
+                  target="_blank"
+                >
+                  Predicto
+                </a>
+              </li>
+            </ul>
+          </div>
+        </Col>
+      </Row>
+    </AntFooter>
+  );
+}
+
+export default Footer;
